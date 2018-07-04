f2a2b7d906232e27f3ea49f5d700d06b7b099c1a
Jochen Schalanda <jochen@graylog.com>
2018-07-04 13:17:53 +0200

    Use sudo to download and chmod Hadolint

--- test/linter.sh
+++ test/linter.sh
@@ -2,8 +2,8 @@
 HADOLINT_VERSION='1.9.0'
 HADOLINT_PATH='/usr/local/bin/hadolint'
 if ! [ -x "$(command -v hadolint)" ]; then
-  wget -O "${HADOLINT_PATH}" "https://github.com/hadolint/hadolint/releases/download/v${HADOLINT_VERSION}/hadolint-Linux-x86_64"
-  chmod +x "${HADOLINT_PATH}"
+  sudo wget -O "${HADOLINT_PATH}" "https://github.com/hadolint/hadolint/releases/download/v${HADOLINT_VERSION}/hadolint-Linux-x86_64"
+  sudo chmod +x "${HADOLINT_PATH}"
 fi
 
 hadolint Dockerfile

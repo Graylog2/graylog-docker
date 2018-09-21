54fc364ee759c6c8a5bd7535153a0a49068ab7fd
Jan Doberstein <jd@jalogis.ch>
2018-09-21 16:47:01 +0200

    update hadolint version

--- test/linter.sh
+++ test/linter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-HADOLINT_VERSION='1.9.0'
+HADOLINT_VERSION='1.13.0'
 HADOLINT_PATH='/usr/local/bin/hadolint'
 if ! [ -x "$(command -v hadolint)" ]; then
   sudo wget -O "${HADOLINT_PATH}" "https://github.com/hadolint/hadolint/releases/download/v${HADOLINT_VERSION}/hadolint-Linux-x86_64"

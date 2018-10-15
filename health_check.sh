c7317dbf9d7b6c813a7e144cded99947c85cbc8c
Bodo Schulz <bodo@boone-schulz.de>
2018-10-15 11:27:04 +0200

    support also https for the rest interface

--- health_check.sh
+++ health_check.sh
@@ -5,10 +5,17 @@ source /etc/profile
 set -eo pipefail
 
 host="$(hostname -i || echo '127.0.0.1')"
+
 port=9000
+tls=false
+proto=http
 
 # rest_listen_uri = http://0.0.0.0:9000/api/
 rest_listen_uri=$(grep "rest_listen_uri" ${GRAYLOG_HOME}/data/config/graylog.conf)
+#rest_enable_tls = true
+tls=$(grep "^rest_enable_tls" ${GRAYLOG_HOME}/data/config/graylog.conf | awk -F '=' '{print $2}' | awk '{$1=$1};1')
+
+[[ ${tls} = "true" ]] && proto=https
 
 if [[ ! -z "${GRAYLOG_WEB_ENDPOINT_URI}" ]]
 then
@@ -18,7 +25,7 @@ then
   port=$(echo -e "${rest_listen_uri}" | awk -F '=' '{print $2}' | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')
 fi
 
-if curl --silent --fail http://${host}:${port}/api
+if curl --silent --fail ${proto}://${host}:${port}/api
 then
   exit 0
 fi

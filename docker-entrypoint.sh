8badeec71d6b9dc067f1a9d3078433ed890a49cf
Bodo Schulz <bodo@boone-schulz.de>
2019-01-21 13:55:34 +0100

    fix entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -35,14 +35,15 @@ setup() {
 
 graylog() {
 
-  "${JAVA_HOME}/bin/java" "${GRAYLOG_SERVER_JAVA_OPTS}" \
+  "${JAVA_HOME}/bin/java" \
+    ${GRAYLOG_SERVER_JAVA_OPTS} \
     -jar \
     -Dlog4j.configurationFile="${GRAYLOG_HOME}/data/config/log4j2.xml" \
     -Djava.library.path="${GRAYLOG_HOME}/lib/sigar/" \
     -Dgraylog2.installation_source=docker \
     "${GRAYLOG_HOME}/graylog.jar" \
     server \
-    -f "${GRAYLOG_HOME}/data/config/graylog.conf" "${GRAYLOG_SERVER_OPTS}"
+    -f "${GRAYLOG_HOME}/data/config/graylog.conf"
 }
 
 run() {

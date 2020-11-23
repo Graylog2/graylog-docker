b998c129c5690578cf9b06083b1b69343a1d2a36
Donald Morton <donald.morton@graylog.com>
2020-11-23 11:20:18 -0600

    Remove crazy sed statements and use vars properly.

--- docker/forwarder/forwarder-entrypoint.sh
+++ docker/forwarder/forwarder-entrypoint.sh
@@ -2,25 +2,6 @@
 
 set -e
 
-if [[ ! -z ${FORWARDER_SERVER_HOSTNAME} ]]
-then
-  echo "adding forwarder_server_hostname to forwarder.conf"
-  sed -i "s/^forwarder_server_hostname =$/forwarder_server_hostname = ${FORWARDER_SERVER_HOSTNAME}/g" /etc/graylog/forwarder/forwarder.conf
-fi
-
-if [[ ! -z ${FORWARDER_GRPC_TLS_TRUST_CHAIN_CERT_FILE} ]]
-then
-  echo "adding forwarder_grpc_tls_trust_chain_cert_file to forwarder.conf"
-  sed -i "s/^forwarder_grpc_tls_trust_chain_cert_file =$/forwarder_grpc_tls_trust_chain_cert_file = ${FORWARDER_GRPC_TLS_TRUST_CHAIN_CERT_FILE//\//\\/}/g" /etc/graylog/forwarder/forwarder.conf
-fi
-
-if [[ ! -z ${FORWARDER_GRPC_API_TOKEN} ]]
-then
-  echo "adding forwarder_grpc_api_token to forwarder.conf"
-  sed -i "s/^forwarder_grpc_api_token =$/forwarder_grpc_api_token = ${FORWARDER_GRPC_API_TOKEN}/g" /etc/graylog/forwarder/forwarder.conf
-fi
-
-
 # Convert all environment variables with names ending in __FILE into the content of
 # the file that they point at and use the name without the trailing __FILE.
 # This can be used to carry in Docker secrets.

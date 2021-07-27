d0531c530ca75bdaf9ac844649b267fe59f77145
Donald Morton <donald.morton@graylog.com>
2021-07-27 17:00:38 -0500

    Escape dollar signs.

--- jenkins.groovy
+++ jenkins.groovy
@@ -28,7 +28,7 @@ pipeline
         sh 'docker run --rm --privileged multiarch/qemu-user-static --reset -p yes'
         sh 'docker buildx create --name multiarch --driver docker-container --use'
         sh 'docker buildx inspect --bootstrap'
-        sh "docker buildx build --platform linux/arm64/v8 --no-cache --build-arg GRAYLOG_VERSION=$(cat VERSION) --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") --tag graylog-multiarch-test:latest --file /tmp/graylog-docker/docker/oss/Dockerfile ."
+        sh "docker buildx build --platform linux/arm64/v8 --no-cache --build-arg GRAYLOG_VERSION=\$(cat VERSION) --build-arg BUILD_DATE=\$(date -u +"%Y-%m-%dT%H:%M:%SZ") --tag graylog-multiarch-test:latest --file /tmp/graylog-docker/docker/oss/Dockerfile ."
       }
    }
 }

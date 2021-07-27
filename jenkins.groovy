32b86d561990c13304eed78d0adb72fbe6e95123
Donald Morton <donald.morton@graylog.com>
2021-07-27 17:02:48 -0500

    Add steps block.

--- jenkins.groovy
+++ jenkins.groovy
@@ -25,10 +25,13 @@ pipeline
           tag pattern: "/^(?:[4-9]|\\d{2,}).[0-9]+.[0-9]+-(?:[0-9]+|alpha|beta|rc).*/", comparator: "REGEXP"
         }
 
-        sh 'docker run --rm --privileged multiarch/qemu-user-static --reset -p yes'
-        sh 'docker buildx create --name multiarch --driver docker-container --use'
-        sh 'docker buildx inspect --bootstrap'
-        sh "docker buildx build --platform linux/arm64/v8 --no-cache --build-arg GRAYLOG_VERSION=\$(cat VERSION) --build-arg BUILD_DATE=\$(date -u +\"%Y-%m-%dT%H:%M:%SZ\") --tag graylog-multiarch-test:latest --file /tmp/graylog-docker/docker/oss/Dockerfile ."
+        steps
+        {
+          sh 'docker run --rm --privileged multiarch/qemu-user-static --reset -p yes'
+          sh 'docker buildx create --name multiarch --driver docker-container --use'
+          sh 'docker buildx inspect --bootstrap'
+          sh "docker buildx build --platform linux/arm64/v8 --no-cache --build-arg GRAYLOG_VERSION=\$(cat VERSION) --build-arg BUILD_DATE=\$(date -u +\"%Y-%m-%dT%H:%M:%SZ\") --tag graylog-multiarch-test:latest --file /tmp/graylog-docker/docker/oss/Dockerfile ."
+        }
       }
    }
 }

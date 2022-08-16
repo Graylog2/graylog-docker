13a3e508a59f2fea74fd18e2a5e1cdb36a8e515e
Marco Pfatschbacher <marco@graylog.com>
2022-08-16 11:45:28 +0200

    Use GRAYLOG_PLUGIN_DIR in find

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -82,8 +82,8 @@ fi
 # Merge plugin dirs to allow mounting of /plugin as a volume
 export GRAYLOG_PLUGIN_DIR=${GRAYLOG_HOME}/plugins-merged
 rm -f ${GRAYLOG_PLUGIN_DIR}/*
-find ${GRAYLOG_HOME}/plugins-default/ -type f -exec cp {} ${GRAYLOG_HOME}/plugins-merged/ \;
-find ${GRAYLOG_HOME}/plugin ! -readable -prune -o -type f -a -readable -exec cp {} ${GRAYLOG_HOME}/plugins-merged/ \;
+find ${GRAYLOG_HOME}/plugins-default/ -type f -exec cp {} ${GRAYLOG_PLUGIN_DIR}/ \;
+find ${GRAYLOG_HOME}/plugin ! -readable -prune -o -type f -a -readable -exec cp {} ${GRAYLOG_PLUGIN_DIR}/ \;
 
 
 setup() {

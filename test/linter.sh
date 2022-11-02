ee29aea90c690c876e16c32d9d00381f90a391b8
Marco Pfatschbacher <marco@graylog.com>
2022-11-02 13:44:10 +0100

    Perform apt upgrade for our base images (#233)
    * Perform apt upgrade for our base images
    
    Also update hadolint to 2.10.0
    
    Merge some RUN commands to accomodate DL3059 but also ignore some
    where it would make the code ugly
    
    * Run hadolint with docker
    
    * fix comment
    
    * use --rm

--- test/linter.sh
+++ test/linter.sh
@@ -1,16 +1,13 @@
 #!/bin/bash -ex
 
 
-# This will work on linux
-# Mac users should before install hadolint via `brew install hadolint`
-	HADOLINT_VERSION='1.13.0'
-	HADOLINT_PATH='/usr/local/bin/hadolint'
-	if ! [ -x "$(command -v hadolint)" ]; then
-	  sudo wget -O "${HADOLINT_PATH}" "https://github.com/hadolint/hadolint/releases/download/v${HADOLINT_VERSION}/hadolint-Linux-x86_64"
-	  sudo chmod +x "${HADOLINT_PATH}"
-	fi
+HADOLINT_VERSION='2.10.0'
 
-# both dockerfiles
+hadolint() {
+	docker run --rm -i hadolint/hadolint:$HADOLINT_VERSION < $1
+}
+
+# lint all dockerfiles
 hadolint docker/oss/Dockerfile
 hadolint docker/enterprise/Dockerfile
 hadolint docker/forwarder/Dockerfile

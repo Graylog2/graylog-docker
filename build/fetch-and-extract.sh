32719e8be0232d72f34f461c5cda2bd501fa277b
Bernd Ahlers <bernd@graylog.com>
2023-02-01 15:14:51 +0100

    Remove wrong download URL and improve fetch script

--- build/fetch-and-extract.sh
+++ build/fetch-and-extract.sh
@@ -4,14 +4,14 @@ mkdir -p /tmp/download
 
 # download
 for url in "$@"; do
-  local=/tmp/download/${url##*/}
+  file=/tmp/download/${url##*/}
 
-  echo "fetching $url ..."
+  echo "fetching: $url"
   curl \
-    --silent \
-    --location \
+    -fsSL \
     --retry 3 \
-    --output $local \
+    --retry-delay 2 \
+    --output "$file" \
     "$url"
 done
 
@@ -26,5 +26,6 @@ done
 mkdir -p /opt/graylog
 
 for file in /tmp/download/*.tgz; do
+  echo "extracting: $file"
   tar --extract --gzip --file "$file" --strip-components=1 --directory /opt/graylog
 done

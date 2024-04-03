948b45146219425cb4f5fc4ba97010b4a38497a3
Bernd Ahlers <bernd@graylog.com>
2024-04-03 16:25:35 +0200

    Update release.py from 6.0

--- release.py
+++ release.py
@@ -14,12 +14,21 @@ parser.add_argument('--get-forwarder-image-version',
                     help="Get Forwarder image version.", action='store_true')
 parser.add_argument('--generate-readme',
                     help="Generate a new README.md with the latest tags", action='store_true')
+parser.add_argument('--bump', dest='bump',
+                    choices=['graylog', 'forwarder'], help="Bump the given version")
+parser.add_argument('--version', dest='version',
+                    help="The new version and revision")
 
 if len(sys.argv) == 1:
     parser.print_help(sys.stderr)
     sys.exit(1)
 
 args = parser.parse_args()
+
+if args.bump and not args.version:
+    parser.error('Missing --version parameter')
+
+version_parsed = None
 
 with open('version.yml', 'r') as version_file:
     version_parsed = yaml.safe_load(version_file)
@@ -42,3 +51,48 @@ with open('version.yml', 'r') as version_file:
 
         with open("README.md", "w") as readme_file:
             readme_file.write(j2_template.render(version_parsed))
+
+    if args.bump == 'graylog':
+        print(f'Bumping {args.bump} to {args.version}')
+
+        # 6.0.0-alpha.1-1 => version="6.0.0", suffixes=["alpha.1", "1"]
+        # 6.0.0           => version="6.0.0", suffixes=[]
+        version, *suffixes = args.version.split('-', 2)
+        # 6.0.0 => major=6, minor=0, patch= 0
+        major, minor, patch = version.split('.', 2)
+
+        suffix = suffixes[0] if len(suffixes) > 0 else None
+        release = suffixes[1] if len(suffixes) > 1 else 1
+
+        version_parsed[args.bump]['major_version'] = major
+        version_parsed[args.bump]['minor_version'] = minor
+        version_parsed[args.bump]['patch_version'] = f'{patch}-{suffix}' if suffix else patch
+        version_parsed[args.bump]['release'] = int(release)
+
+        print(version_parsed[args.bump])
+    if args.bump == 'forwarder':
+        print(f'Bumping {args.bump} to {args.version}')
+
+        # 6.0-alpha.1-1 => version="6.0", suffixes=["alpha.1", "1"]
+        # 6.0           => version="6.0", suffixes=[]
+        version, *suffixes = args.version.split('-', 2)
+        suffix = suffixes[0] if len(suffixes) > 0 else None
+        release = suffixes[1] if len(suffixes) > 1 else 1
+
+        version_parsed[args.bump]['version'] = f'{version}-{suffix}' if suffix else version
+        version_parsed[args.bump]['release'] = int(release)
+
+        print(version_parsed[args.bump])
+
+
+if version_parsed and args.bump:
+    with open('version.yml', 'w') as f:
+        yaml.dump(version_parsed, f, sort_keys=False)
+
+    with open('version.yml', 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        # Preserve some comments
+        f.write('# For pre-releases: patch_version=0-beta.1, patch_version=0-rc.1\n')
+        f.write('# For GA releases:  patch_version=0\n')
+        f.write(content)
